960c72b974914da975d8e0a6dd4b23fc8bf6034f
Jhonder Natera <jhonder.natera@gmail.com>
2018-11-26 00:47:23 -0400

    Actualizar config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
 	port : process.env.PORT || 3000,
-	db   : 'mongodb://admin:mongo10@ds021689.mlab.com:21689/db-diplomado'
+	db   : 'mongodb://jhonder:m123456@ds021689.mlab.com:21689/db-diplomado'
 }
 
 // process.env.MONGODB || 'mongodb://localhost:27017/shop'
