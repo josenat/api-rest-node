32f9befea7a1024b9159bf558c1beeb98d003ec4
Jhonder Natera <jhonder.natera@gmail.com>
2018-11-26 01:35:12 -0400

    make it better

--- config.js
+++ config.js
@@ -1,7 +1,6 @@
 module.exports = {
 	port : process.env.PORT || 3000,
-	db   : 'mongodb://ds021689.mlab.com:21689/db-diplomado'
-	//db   : 'mongodb://jhonder:m123456@ds021689.mlab.com:21689/db-diplomado'
+	db   : 'mongodb://jhonder:m123456@ds021689.mlab.com:21689/db-diplomado'
 	//db   : process.env.MONGODB || 'mongodb://localhost:27017/shop'
 }
  
